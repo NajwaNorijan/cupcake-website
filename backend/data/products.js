2705edae2807c5fb594f5b8ddbec63e317391585
Najwa Norijan <najwanorijan@gmail.com>
2021-05-11 02:14:53 +0800

    Initial commit

---
+++ backend/data/products.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+      name: "Chocolate Cupcake",
+      imageUrl:
+        "https://www.lifeloveandsugar.com/wp-content/uploads/2017/05/Moist-Homemade-Chocolate-Cupcakes2.jpg",
+      description:
+        "Chocolate",
+      price: 6,
+      countInStock: 15,
+    },
+    {
+      name: "Vanilla Cupcake",
+      imageUrl:
+        "https://natashaskitchen.com/wp-content/uploads/2020/05/Vanilla-Cupcakes-3.jpg",
+      description:
+        "Vanilla",
+      price: 5,
+      countInStock: 10,
+    },
+    {
+      name: "Red Velvet Cupcake",
+      imageUrl:
+        "https://therecipecritic.com/wp-content/uploads/2017/01/RedVelvetCupcakes2.jpg",
+      description:
+        "Red Velvet",
+      price: 8,
+      countInStock: 5,
+    },
+    {
+      name: "Butterscoth Cookies Cupcake",
+      imageUrl:
+        "https://i1.wp.com/whatjessicabakednext.com/wp-content/uploads/2019/08/butterscotch-cupcake-recipe.png?resize=550%2C826&ssl=1",
+      description:
+        "Butterscoth Cookies",
+      price: 10,
+      countInStock: 25,
+    },
+    {
+      name: "Walnut Carrot Cupcake",
+      imageUrl:
+        "https://www.inspiredtaste.net/wp-content/uploads/2020/05/Carrot-Cupcakes-Recipe-2-1200.jpg",
+      description:
+        "Walnut Carrot",
+      price: 12,
+      countInStock: 4,
+    },
+    {
+      name: "Mango Cupcake",
+      imageUrl:
+        "https://i.pinimg.com/originals/10/6f/25/106f25d7ccdff504d1b89056514f93b3.jpg",
+      description:
+        "Mango",
+      price: 12,
+      countInStock: 10,
+    },
+  ];
+  
+  module.exports = products;
+  
